8746d915dd5a47ba17888ff60d259412d934d108
Jackson-Wozniak <jjw30@comcast.net>
2024-03-21 14:43:10 -0400

    make EventProcessor a component so that Fighters can be updated after an Event is simulated

--- backend/src/main/java/internal/mma_league/events/processors/EventProcessor.java
+++ backend/src/main/java/internal/mma_league/events/processors/EventProcessor.java
@@ -3,17 +3,26 @@ package internal.mma_league.events.processors;
 import internal.mma_league.events.entity.Event;
 import internal.mma_league.events.entity.EventResult;
 import internal.mma_league.events.entity.FightResult;
+import internal.mma_league.fighters.service.FighterService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@AllArgsConstructor
 public class EventProcessor {
+
+    private final FighterService fighterService;
 
     //order of fight simulation doesn't matter because order is only relevant client-side
     //this means that the simulation of matchups can be done in any order
-    public static EventResult simulateEvent(Event event){
+    public EventResult simulateEvent(Event event){
         EventResult result = new EventResult(event.getName(), event.getDate());
 
         event.getMatchups().forEach(matchup -> {
             FightResult fight = FightProcessor.simulateFight(matchup);
             result.addFight(fight);
+
+            //TODO: update ELO, Record, etc. for both Fighters
         });
 
         return result;
